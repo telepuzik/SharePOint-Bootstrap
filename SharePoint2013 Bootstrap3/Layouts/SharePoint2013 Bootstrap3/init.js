84588086f9f0ed067dd36a2d0e62e9f7a6969dec
Ilya <ilya.melnikov@indusoft.ru>
2016-05-20 11:02:49 +0300

    commit

--- SharePoint2013 Bootstrap3/Layouts/SharePoint2013 Bootstrap3/init.js
+++ SharePoint2013 Bootstrap3/Layouts/SharePoint2013 Bootstrap3/init.js
@@ -1,15 +1,18 @@
-﻿require.config({
-    baseUrl: '',
-    paths: {
-        jquery: '/_layouts/15/SharePoint2013 Bootstrap3/jQuery.2.2.2/Content/Scripts/jquery-2.2.2.min'
-    }
-});
+﻿//require.config({
+//    baseUrl: '',
+//    paths: {
+//        jquery: '/_layouts/15/SharePoint2013 Bootstrap3/jQuery.2.2.2/Content/Scripts/jquery-2.2.2.min'
+//    }
+//});
 
-require(['jquery'], function () {
-    console.log("JS: jquery loaded");
-    loadCss("/_layouts/15/SharePoint2013 Bootstrap3/bootstrap.3.3.6/content/content/bootstrap.css");
-    loadCss("/_layouts/15/SharePoint2013 Bootstrap3/bootstrap3_reset.css");
-});
+//require(['jquery'], function () {
+//    console.log("JS: jquery loaded");
+//    loadCss("/_layouts/15/SharePoint2013 Bootstrap3/bootstrap.3.3.6/content/content/bootstrap.css");
+//    loadCss("/_layouts/15/SharePoint2013 Bootstrap3/bootstrap3_reset.css");
+//});
+
+loadCss("/_layouts/15/SharePoint2013 Bootstrap3/bootstrap.3.3.6/content/content/bootstrap.css");
+loadCss("/_layouts/15/SharePoint2013 Bootstrap3/bootstrap3_reset.css");
 
 function loadCss(url) {
     var link = document.createElement("link");
--- set_bootstrapmasterpage.ps1
+++
@@ -1,5 +0,0 @@
-$site = Get-SPweb "http://localhost"
-$site.MasterUrl = "/_catalogs/masterpage/seattle_bootstrap.master"
-$site.CustomMasterUrl = "/_catalogs/masterpage/seattle_bootstrap.master"
-$site.Update()
-$site.Dispose()
